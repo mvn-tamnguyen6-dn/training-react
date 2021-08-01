0edcbac5e70f80cb2452968fb583cd9f65709c86
mvn-tamnguyen6-dn <tam.nguyen6@monstar-lab.com>
2021-08-01 17:06:52 +0700

    exe06

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
+import Users from './Users'
 import React from 'react'
 
 class Main extends React.Component {
   render() {
     return (
-      <main class="page-main">
-        <div class="container">
-          <div class="main-content text-center">
-            <a href="/">
-              <img src={logo} class="react-logo" alt="react-logo"/>
-            </a>
-            <h4 class="text-center">Hello React!</h4>
-          </div>
+      <main className="page-main">
+        <div className="container">
+          <Users />
         </div>
       </main>
     );
